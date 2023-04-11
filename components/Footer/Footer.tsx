4a669c8f36af297fde41dc3edc3d22433d1b8cbc
farshad-vgr <farshadvaghari@gmail.com>
2023-04-11 20:11:19 +0330

    Header media query edited

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,9 +4,11 @@ export default function Footer() {
 	return (
 		<>
 			<footer className="w-full h-[40rem] bg-footerBackground bg-cover bg-[center_left_35%] bg-no-repeat">
-				<Container>
-					<h1 className="text-2xl">This is my Footer section</h1>
-				</Container>
+				<div className="w-full h-[40rem] text-white bg-black bg-opacity-30">
+					<Container>
+						<h1 className="text-2xl">This is my Footer section</h1>
+					</Container>
+				</div>
 			</footer>
 		</>
 	);
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,26 +1,9 @@
-import { useEffect, useState } from "react";
 import Image from "next/image";
 
 import { Container, Navbar, SearchButton, MenuButton } from "../index";
 import LogoImage from "../../public/assets/images/logo.png";
 
 export default function Header() {
-	const [isDesktop, setIsDesktop] = useState(true);
-
-	useEffect(() => {
-		window.innerWidth < 640 ? setIsDesktop(false) : setIsDesktop(true);
-
-		window.addEventListener("resize", () => {
-			window.innerWidth < 640 ? setIsDesktop(false) : setIsDesktop(true);
-		});
-
-		return () => {
-			window.removeEventListener("resize", () => {
-				window.innerWidth < 640 ? setIsDesktop(false) : setIsDesktop(true);
-			});
-		}
-	}, []);
-
 	return (
 		<>
 			<header className={`w-full h-[15vh] sm:h-[20vh] bg-headerBackground bg-cover bg-[center_right_35%] bg-no-repeat bg-opacity-20`}>
@@ -30,9 +13,17 @@ export default function Header() {
 							<Image src={LogoImage} alt="Logo" />
 						</div>
 
-						<div className="mt-14">{isDesktop && <Navbar direction="flex-row" />}</div>
+						<div className="mt-14 hidden sm:block">
+							<Navbar direction="flex-row" />
+						</div>
 
-						<div>{isDesktop ? <SearchButton /> : <MenuButton />}</div>
+						<div className="hidden sm:block">
+							<SearchButton />
+						</div>
+
+						<div className="block sm:hidden">
+							<MenuButton />
+						</div>
 					</Container>
 				</div>
 			</header>
