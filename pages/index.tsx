35f2cc636db98d6bce0a288c710d5c602d828a81
farshad-vgr <farshadvaghari@gmail.com>
2023-05-22 15:03:27 +0330

    useRoute hook edited

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-interface Props {
-	cities: {
-		id: number;
-		name: string;
-		province: string;
-	}[];
-}
+// interface Props {
+// 	cities: {
+// 		id: number;
+// 		name: string;
+// 		province: string;
+// 	}[];
+// }
 
 let basePath: string;
 
-export default function Home({ cities }: Props) {
-	console.log(cities);
+export default function Home() {
+	// console.log(cities);
 	const router = useRouter();
-	// console.log(router.basePath === "");
 	basePath = router.basePath;
+	console.log(router.basePath, basePath);
 
 	return (
 		<>
@@ -28,18 +28,18 @@ export default function Home({ cities }: Props) {
 	);
 }
 
-export async function getStaticProps() {
-	// Fetching data from internal "API route"
-	const response =
-		basePath === "https://fv-travelo-iran.vercel.app/"
-			? await fetch(`https://fv-travelo-iran.vercel.app/api/cities?country=iran`)
-			: await fetch(`http://localhost:3000/api/cities?country=iran`);
-	// const response = await fetch(`http://localhost:3000/api/cities?country=iran`);
-	const cities = await response.json();
+// export async function getStaticProps() {
+// 	// Fetching data from internal "API route"
+// 	const response =
+// 		basePath === "https://fv-travelo-iran.vercel.app/"
+// 			? await fetch(`https://fv-travelo-iran.vercel.app/api/cities?country=iran`)
+// 			: await fetch(`http://localhost:3000/api/cities?country=iran`);
+// 	// const response = await fetch(`http://localhost:3000/api/cities?country=iran`);
+// 	const cities = await response.json();
 
-	return {
-		props: {
-			cities,
-		},
-	};
-}
+// 	return {
+// 		props: {
+// 			cities,
+// 		},
+// 	};
+// }
