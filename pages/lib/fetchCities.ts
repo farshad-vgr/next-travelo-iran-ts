d339868caba0e593ce47ea53f099b40c39331278
farshad-vgr <farshadvaghari@gmail.com>
2023-05-22 13:56:29 +0330

    lib file not async

--- pages/lib/fetchCities.ts
+++ pages/lib/fetchCities.ts
@@ -1,5 +1,5 @@
 // If you are using "API routes" to fetch data, the logic for fetching the data from the CMS can be shared by using a lib/ directory
-export default async function fetchCities(country: string) {
+export async function fetchCities(country: string) {
 	const response = await fetch(`http://localhost:3000/api/cities?country=${country}`);
 	const data = await response.json();
 
